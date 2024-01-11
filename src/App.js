3b209b8cd740819c770bdfc08f4485e760c9ba9f
josezanle <jose.rios.lm@gmail.com>
2024-01-11 20:36:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
+import { Water } from 'three/addons/objects/Water.js';
+import { Sky } from 'three/addons/objects/Sky.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  let container;
+  let camera, scene, renderer;
+  let water, sun;
+  let renderTarget;
+
+
+  function init() {
+    container = document.getElementById('container');
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.5;
+    container.appendChild(renderer.domElement);
+
+    sun = new THREE.Vector3();
+    scene = new THREE.Scene();
+    const sky = new Sky();
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    const sceneEnv = new THREE.Scene();
+    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
+    camera.position.set(30, 30, 100);
+
+
+    water = new Water(
+      waterGeometry,
+      {
+        textureWidth: 512,
+        textureHeight: 512,
+        waterNormals: new THREE.TextureLoader().load('./images/waternormals.jpg', function (texture) {
+          texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        }),
+        sunDirection: new THREE.Vector3(),
+        sunColor: 0xffffff,
+        waterColor: 0x001e0f,
+        distortionScale: 3.7,
+        fog: scene.fog !== undefined,
+      }
+    );
+
+    water.rotation.x = -Math.PI / 2;
+
+    scene.add(water);
+
+    
+    sky.scale.setScalar(10000);
+    scene.add(sky);
+
+    const skyUniforms = sky.material.uniforms;
+
+    skyUniforms['turbidity'].value = 10;
+    skyUniforms['rayleigh'].value = 2;
+    skyUniforms['mieCoefficient'].value = 0.005;
+    skyUniforms['mieDirectionalG'].value = 0.8;
+
+    const parameters = {
+      elevation: 2,
+      azimuth: 180,
+    };
+
+    function updateSun() {
+      const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+      const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+      sun.setFromSphericalCoords(1, phi, theta);
+
+      sky.material.uniforms['sunPosition'].value.copy(sun);
+      water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+
+      if (renderTarget !== undefined) renderTarget.dispose();
+
+      sceneEnv.add(sky);
+      renderTarget = pmremGenerator.fromScene(sceneEnv);
+      scene.add(sky);
+
+      scene.environment = renderTarget.texture;
+    }
+
+    updateSun();
+
+    window.addEventListener('resize', onWindowResize);
+    onWindowResize();
+  }
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
+
+  function render() {
+    water.material.uniforms['time'].value += 1.0 / 60.0;
+    renderer.render(scene, camera);
+  }
+
+  useEffect(() => {
+    init();
+    animate();
+
+    return () => {
+      document.getElementById('container').removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return <div id='container'></div>;
+};
 
 export default App;
